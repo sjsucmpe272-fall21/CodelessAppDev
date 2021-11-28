a2a9ec72e2c8412614febbe215ca81b708a981e6
somil <somilj94@gmail.com>
2021-11-27 20:59:14 -0800

    funcionality 2 front-end prototype

--- fe-cmpe-272/src/DefinedRoutes.js
+++ fe-cmpe-272/src/DefinedRoutes.js
@@ -3,6 +3,7 @@ import Homepage from "./components/Homepage";
 import Login from "./components/Login";
 import LandingPage from "./components/LandingPage";
 import Func1 from "./components/Func1";
+import Func2 from "./components/Func2";
 
 export default function DefinedRoutes() {
   return (
@@ -12,6 +13,7 @@ export default function DefinedRoutes() {
         {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/landingpage" element={<LandingPage />} />
         <Route path="/func1" element={<Func1 />} />
+        <Route path="/func2" element={<Func2 />} />
       </Routes>
     </BrowserRouter>
   );
